f123aa0dbc2a185ea5629a26e3afab31b6195f21
Jocelyn Hua <jocelynhua22@gmail.com>
2024-05-13 18:55:32 -0700

    L7: Complete

--- src/Pages/CourseWork/Essays/Essays.jsx
+++ src/Pages/CourseWork/Essays/Essays.jsx
@@ -15,7 +15,22 @@ const Essays = () => {
             <SingleEssay
                 question={`Q2: What's the difference between Git and Github?`}
             >
-                Answer 2 goes here
+                <strong>Git</strong> is a free, open-source software distributed
+                version control system designed to manage all source code
+                history during software development. It is designed for
+                coordinating work between programmers, but it can be used to
+                keep a history of commits, reverse changes, and let developers
+                share code. <strong>GitHub </strong> is a collaboration and
+                version control platform for storing and managing code. Using
+                this tool, you can collaborate with others on projects from
+                anywhere. The main <strong>Git</strong> vs.{" "}
+                <strong>GitHub </strong>
+                difference is in their functionality. While they both provide
+                source code management and make merging and sharing code easier,
+                that is pretty much where their similarities end. Think of Git
+                as a single computer and GitHub as a network of multiple
+                interconnected computers, all with the same end goal but a
+                wildly different role for how to get there.
             </SingleEssay>
             <SingleEssay
                 question={`Q3: What is the difference between JQuery and React?`}
@@ -42,14 +57,36 @@ const Essays = () => {
                 you perform more sophisticated functions and principally
                 highlights IU development, DOM manipulation, and the likes.
             </SingleEssay>
-            <SingleEssay question={`Q4: Sample Question...`}>
-                Answer 4 goes here
+            <SingleEssay
+                question={`Q4: What is the difference between a Front-End and Back-end Developer?`}
+            >
+                <strong>Front-end developers</strong> focus on the user-facing
+                side of the website. They design the visual aspect of websites
+                for users to interact with, including colors, layout, and fonts.
+                Front-end developers ensure that visitors can easily interact
+                with and navigate sites by using programming languages, design
+                skills, and other tools. Whereas{" "}
+                <strong>Back-end developers</strong> focus on the server side of
+                websites. They use technical skills to perform the
+                behind-the-scenes work that creates a website’s structure and
+                overall functionality, allowing a site’s front end to exist.
+                They create a site’s operations, databases, and application
+                programming interface (API).
             </SingleEssay>
-            <SingleEssay question={`Q5: Sample Question...`}>
+            <SingleEssay
+                question={`Q5: In React, what are the different ways we can link to resources (e.g. Root Relative vs. Document Relative)?`}
+            >
                 Answer 5 goes here
             </SingleEssay>
-            <SingleEssay question={`Q6: Sample Question...`}>
+            <SingleEssay
+                question={`Q6: What is the difference between jpg, gif, png and svg images?`}
+            >
                 Answer 6 goes here
+            </SingleEssay>
+            <SingleEssay
+                question={`Q7: Define the following Agile roles: Product Manager, Scrum Master, UX Designer, Developer/Engineer, QA/SDET, and DevOps.`}
+            >
+                Answer 7 goes here
             </SingleEssay>
         </EssaysStyled>
     );
